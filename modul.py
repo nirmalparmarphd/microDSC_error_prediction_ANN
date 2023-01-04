2c5912cc6ee230e51a7c5a9ad17f29b1aeba538b
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-01-04 16:49:12 +0100

    reverted to ANN-reg

--- modul.py
+++ modul.py
@@ -17,40 +17,33 @@ print('-'*70)
 class dsc_error_model():
   def __init__(self,Ref,Sam):
     stable_ml = 0.8 # ml to vol_fraction conversion 0.8-ml=1 
-    self.Ref = Ref
-    self.Sam = Sam
+    self.Ref = Ref/stable_ml 
+    self.Sam = Sam/stable_ml
     model = load_model('micro_dsc_dl.h5')
     scaler = joblib.load('scaler.pkl')
-    list = [2.  , 1.67, 3.33, 0.09, 0.6 , 0.72, 0.25, 0.42, 1.2 , 0.5 , 0.83,0.36, 0.15, 0.18, 1. , 0.3 ]
-    self.vol_rel = np.round(((Ref*Ref)/Sam),2)
-    
-    for idx, item in enumerate(list):
-      if item / self.vol_rel == 1:
-          list[idx] = 1 
-      else:
-          list[idx] = 0 
-          
-    print(list)
-    #self.list_ = self.list
-    data = [self.Ref, self.Sam, self.vol_rel] + list
-    print(data)
+    data = [self.Ref, self.Sam, self.Ref/self.Sam]
     data = pd.DataFrame([data])
     data_ = scaler.transform(data)
     pred = model.predict(data_)
     pred_ = np.round(((pred*100)-100).astype(np.float64),2)
- 
+    
     print('-'*70)
     print('Reference amount [ml]: ', Ref)
     print('Sample amount [ml]: ', Sam)
-
+    
     if abs(pred_) <= 1.5:
-      print('Heat capacity measurement deviation prediction (%): ')
-      print(pred_)
-     
+      print('Heat capacity measurement deviation prediction (%): ', pred_)
+      print('''COMMENT(s): 
+            Sample and reference amount combination is appropriate.
+            Consider 0.8~ml as standard amount to avoid any deviation in the measurement.''')
     else:
-      print('Heat capacity measurement deviation prediction (%): ')
-      print(pred_)
-      
+      print('Heat capacity measurement deviation prediction (%): ', pred_)
+      print('''COMMENT(s): 
+            Sample and reference amount maybe not appropriate.
+            The reference material may too low!
+            Re-run calibration with the fully filled reference material in a batch cell to avoid a large deviation.
+            Consider 0.8~ml as standard amount to avoid any deviation in the measurement.
+            ''')
     print('-'*70)
     
     
