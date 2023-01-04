78201b6cef3f7994fcbc0c76dd2265fe70c56bdd
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-01-04 17:31:55 +0100

    module update - removed ml info

--- modul.py
+++ modul.py
@@ -7,7 +7,7 @@ print('-'*70)
 information ='''
 * This is a Deep learning (DL) ANN model to predict a deviation due to an inappropriate amount combination of the sample and a reference material in a batch cell of Tian-Calvet micro-DSC.
 
-* Predict the possible deviation that may arise in the heat capacity measurement experiment with the sample and a reference material amount in [ml]!
+* Predict the possible deviation that may arise in the heat capacity measurement experiment with the sample and a reference material amount!
 
 -->ANN Model accuracy on the test data is 99.62 [%]<--'''
 
@@ -15,10 +15,9 @@ print(information)
 print('-'*70)
 
 class dsc_error_model():
-  def __init__(self,Ref,Sam):
-    stable_ml = 0.8 # ml to vol_fraction conversion 0.8-ml=1 
-    self.Ref = Ref/stable_ml 
-    self.Sam = Sam/stable_ml
+  def __init__(self,Ref,Sam): 
+    self.Ref = Ref
+    self.Sam = Sam
     model = load_model('micro_dsc_dl.h5')
     scaler = joblib.load('scaler.pkl')
     data = [self.Ref, self.Sam, self.Ref/self.Sam]
@@ -28,8 +27,8 @@ class dsc_error_model():
     pred_ = np.round(((pred*100)-100).astype(np.float64),1)
     
     print('-'*70)
-    print('Reference amount [ml]: ', Ref)
-    print('Sample amount [ml]: ', Sam)
+    print('Reference amount : ', Ref)
+    print('Sample amount : ', Sam)
     
     if abs(pred_) <= 1.5:
       print('Heat capacity measurement deviation prediction (%): ', pred_)
