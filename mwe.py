2b47c40ca5f73d02bedc7a4b4f89a101ae5b090b
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-01-04 17:40:24 +0100

    mwe corrected ap per amount fraction

--- mwe.py
+++ mwe.py
@@ -1,16 +1,21 @@
+# prediction of error/deviation in the heat capacity measurement
+# use: prediction = dsc_error_model(Reference amount, Sample amount)
+# NOTE: enter the sample and reference material amount as mentioned below
+    ## Full cell:               1.0 
+    ## Two Third full cell:     0.66
+    ## One half full cell:      0.5
+    ## One third full cell:     0.33
+
+
 ### MINIMUM WORKING EXAMPLE ###
 
 # import module
 from modul import *
 
-# prediction of error/deviation in the heat capacity measurement
-# use: prediction = dsc_error_model(Reference amount(ml), Sample amount(ml))
-# NOTE: enter the sample and reference material amount in [ml] 
-
-# Example 1: Reference amount(ml) = 0.8, Sample amount(ml)= 0.8
-# 0.8~ml [full], 0.4~ml [half full], 0.26~ml [one third full]
-Reference_amount = 1
+# defining values
+Reference_amount = 0.3
 Sample_amount = 1
 
+# prediction of deviation in heat capacity measurement
 error_pred = dsc_error_model(Reference_amount,Sample_amount)
 
