c81153ff4c1a471d50f68b189f08d8bd873f2674
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-01-07 18:10:15 +0100

    mwe info updated

--- mwe.py
+++ mwe.py
@@ -1,16 +1,15 @@
 # prediction of error/deviation in the heat capacity measurement
 # use: prediction = dsc_error_model(Reference amount, Sample amount)
 # NOTE: enter the sample and reference material amount as mentioned below
+
     ## Full cell:               1.0 
     ## Two Third full cell:     0.66
     ## One half full cell:      0.5
     ## One third full cell:     0.33
 
-
 ### MINIMUM WORKING EXAMPLE ###
 
 # import module
-
 from modul import *
 
 # defining values
@@ -19,4 +18,3 @@ Sample_amount = 1
 
 # prediction of deviation in heat capacity measurement
 error_pred = dsc_error_model(Reference_amount,Sample_amount)
-
